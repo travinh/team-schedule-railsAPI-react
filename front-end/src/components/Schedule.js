bda19710846522b6c423fee3bdb120dd07bd1df8
travinh <vinh.v.tran@ucdenver.edu>
2020-07-15 07:11:31 -0600

    use useState  for schedule component

--- front-end/src/components/Schedule.js
+++ front-end/src/components/Schedule.js
@@ -1,47 +1,55 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-export default class Schedule extends Component {
+// export default class Schedule extends Component {
 
-    state = {
-        num_member:0
-    }
+//     state = {
+//         num_member:0
+//     }
 
-    addMember = () => {
-        // console.log(this)
-        this.setState((prevState) => {
-            return {num_member: prevState.num_member +1}
-        })
-    }
+//     addMember = () => {
+//         // console.log(this)
+//         this.setState((prevState) => {
+//             return {num_member: prevState.num_member +1}
+//         })
+//     }
 
     
 
-    render() {
-        return (
-            <div>
-                <p>Title: {this.props.schedule.title} </p>
-                <p>Content: {this.props.schedule.content}</p>
-                <p>Member: {this.state.num_member} </p>
-                <p>User ID: {this.props.schedule.user_id}</p>
-                <button onClick={this.addMember}>+</button>
-                <button onClick={() => this.props.remove(this.props.schedule.id)}>Delete</button>
-                <hr/>
-            </div>
-        )
-    }
-}
-
-
-
-// export default function Schedule({title,content, num_member,user_id}) {
-//     return (
-//         <div>
-//             <p>Title: {title} </p>
-//             <p>Content: {content}</p>
-//             <p>Num Member: {num_member}</p>
-//             <p>User ID: {user_id}</p>
-//         <hr/>
-            
-//         </div>
-//     )
+//     render() {
+//         return (
+//             <div>
+//                 <p>Title: {this.props.schedule.title} </p>
+//                 <p>Content: {this.props.schedule.content}</p>
+//                 <p>Member: {this.state.num_member} </p>
+//                 <p>User ID: {this.props.schedule.user_id}</p>
+//                 <button onClick={this.addMember}>+</button>
+//                 <button onClick={() => this.props.remove(this.props.schedule.id)}>Delete</button>
+//                 <hr/>
+//             </div>
+//         )
+//     }
 // }
+
+
+
+export default function Schedule({schedule:{id,title,content,user_id},remove}) {
+
+    const [num_member, setNumMember] = useState(0)
+
+
+
+    return (
+        <div>
+            <p>Title: {title} </p>
+            <p>Content: {content}</p>
+            <p>Member: {num_member} </p>
+            <p>User ID: {user_id}</p>
+            <button onClick={()=> {setNumMember(num_member+1)}}>+</button>
+            <button onClick={() => remove(id)}>Delete</button>
+
+        <hr/>
+            
+        </div>
+    )
+}
 
