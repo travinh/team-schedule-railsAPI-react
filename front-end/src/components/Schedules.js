d758c231ce4cc9f9e539ab66881e017a1ed59af9
travinh <vinh.v.tran@ucdenver.edu>
2020-07-15 05:29:31 -0600

    creatting schedules and schedule class

---
+++ front-end/src/components/Schedules.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Schedules extends Component{
+
+
+    render(){
+        return(
+            <p>SCHEDULES Page</p>
+        )
+    }
+}
+
+export default Schedules
