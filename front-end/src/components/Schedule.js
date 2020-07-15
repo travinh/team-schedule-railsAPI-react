57d20a1e4ead53aa92c4013941dc2a24072981c8
travinh <vinh.v.tran@ucdenver.edu>
2020-07-15 06:08:36 -0600

    add num_membeer button to schedule

--- front-end/src/components/Schedule.js
+++ front-end/src/components/Schedule.js
@@ -1,31 +1,41 @@
 import React, { Component } from 'react'
 
-// export default class Schedule extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 <p>Title: {this.props.schedule.title} </p>
-//                 <p>Content: {this.props.schedule.content}</p>
-//                 <p>Num Member: {this.props.schedule.num_member}</p>
-//                 <p>User ID: {this.props.schedule.user_id}</p>
-//                 <hr/>
-//             </div>
-//         )
-//     }
-// }
+export default class Schedule extends Component {
 
+    state = {
+        num_member:0
+    }
 
+    addMember(){
+        console.log("add member")
+    }
 
-export default function Schedule({title,content, num_member,user_id}) {
-    return (
-        <div>
-            <p>Title: {title} </p>
-            <p>Content: {content}</p>
-            <p>Num Member: {num_member}</p>
-            <p>User ID: {user_id}</p>
-        <hr/>
-            
-        </div>
-    )
+    render() {
+        return (
+            <div>
+                <p>Title: {this.props.schedule.title} </p>
+                <p>Content: {this.props.schedule.content}</p>
+                <p>Member: {this.state.num_member} </p>
+                <p>User ID: {this.props.schedule.user_id}</p>
+                <button onClick={this.addMember}>+</button>
+                <hr/>
+            </div>
+        )
+    }
 }
+
+
+
+// export default function Schedule({title,content, num_member,user_id}) {
+//     return (
+//         <div>
+//             <p>Title: {title} </p>
+//             <p>Content: {content}</p>
+//             <p>Num Member: {num_member}</p>
+//             <p>User ID: {user_id}</p>
+//         <hr/>
+            
+//         </div>
+//     )
+// }
 
--- front-end/src/components/Schedules.js
+++ front-end/src/components/Schedules.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react'
 import Schedule from './Schedule'
 
-const schedules = [{title: "appointment",content: "with team",num_member: "2",user_id: 1},
-{title: "meeting",content: "with customer",num_member: "4",user_id: 2}]
+const schedules = [{title: "appointment",content: "with team",user_id: 1},
+{title: "meeting",content: "with customer",user_id: 2}]
 
 class Schedules extends Component{
 
@@ -10,7 +10,7 @@ class Schedules extends Component{
     render(){
         return(
             <>
-                {schedules.map(schedule => <Schedule {...schedule}/>)}
+                {schedules.map(schedule => <Schedule schedule={schedule}/>)}
             </>
         )
     }
