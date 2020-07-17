2b7fcd5f5c4d76e09cb2267c4378b001bbb37f2f
travinh <vinh.v.tran@ucdenver.edu>
2020-07-17 01:53:04 -0600

    add postSchedule reducer

--- front-end/src/reducers/index.js
+++ front-end/src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux'
 import schedules from './scheduleReducer'
 import selectedSchedule from './selectedSchedule'
+import postSchedule from './postSchedule'
 
 export default combineReducers({
     schedules,
-    selectedSchedule
+    selectedSchedule,
+    postSchedule
 })
---
+++ front-end/src/reducers/postSchedule.js
@@ -0,0 +1,9 @@
+export default function postSchedule(state={}, action){
+    switch(action.type){
+        case "SET_SCHEDULE":
+            return action.payload
+        default:
+            return state
+    }
+  
+  }
